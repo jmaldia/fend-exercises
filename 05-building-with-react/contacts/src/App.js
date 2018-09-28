5815882d17e80ad0e6d575b728b5640318486338
Jon Maldia <jon@maldia.net>
2018-09-28 13:04:22 -0400

    Add styles and structure to list

--- 05-building-with-react/contacts/src/App.js
+++ 05-building-with-react/contacts/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
 const contacts = [
   {
@@ -24,8 +25,10 @@ const contacts = [
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>   
-    );
+      <div> 
+        <ListContacts contacts={contacts} /> 
+      </div>
+    )
   }
 }
 
