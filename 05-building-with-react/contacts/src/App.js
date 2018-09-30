6cc9d71a563e4cc2de6702063b9cdbc6ff7a7b4e
Jon Maldia <jon@maldia.net>
2018-09-29 20:24:59 -0400

    Add a create contact button

--- 05-building-with-react/contacts/src/App.js
+++ 05-building-with-react/contacts/src/App.js
@@ -5,7 +5,7 @@ import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
   state = {
-    screen: 'create', // list, create
+    screen: 'list', // list, create
     contacts: []
   }
 
